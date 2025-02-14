8987f733f8876bf27ed6b5a4e942b449a96d3c9d
Kasam Thapa Magar <kasamthapa89@gmail.com>
2025-02-15 01:05:46 +0545

    added prisma and tdn

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function DashboardLayout({
-  children,
+  children, 
 }: Readonly<{
   children: React.ReactNode;
 }>) {
